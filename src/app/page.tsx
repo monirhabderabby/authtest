0f8236996e2a0281be214bf980d64a031073919b
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2025-01-28 22:35:05 +0600

    sfd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 import { auth } from "@/auth";
 
+async function getData() {
+  try {
+    const res = await fetch(`http://localhost:5000/testing`).then((res) =>
+      res.json()
+    );
+
+    console.log("SUCCESS FROM GET API CALL: path:/", res);
+
+    return res;
+  } catch (error: any) {
+    console.log("GET ERROR FAILED:", error.message);
+  }
+}
+
 const page = async () => {
   const currentUser = await auth();
-  return <div>{JSON.stringify(currentUser)}</div>;
+  const a = await getData();
+  return (
+    <div>
+      {JSON.stringify(currentUser)} <p>{JSON.stringify(a)}</p>
+    </div>
+  );
 };
 
 export default page;
